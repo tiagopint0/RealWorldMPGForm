e77304c2562049b3435577798b7a3fd8dc30a007
Tiago Pinto <121043025+tiagopint0@users.noreply.github.com>
2025-03-14 03:15:19 +0000

    add new requirement

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
+import snowflake
 
 st.title("Real World Fuel Consumption")
 st.write(
     "For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-conn = st.connection("snowflake")
+conn = st.connection("snowflake", type="snowflake")
 df = conn.query("SELECT * FROM CAR_MODELS;", ttl="10m")
 
 st.write(df)
